2bd0bf9883b2b2ae8a5e885af1a4c37c63c90e09
wiggam <107154769+wiggam@users.noreply.github.com>
2023-08-04 14:25:10 -0400

    Create auth.py

---
+++ auth.py
@@ -0,0 +1,25 @@
+import tweepy
+from keys import api_key, api_secret, access_token, access_token_secret
+
+def get_twitter_conn_v1() -> tweepy.API:
+    """Get twitter conn 1.1"""
+
+    auth = tweepy.OAuth1UserHandler(api_key, api_secret)
+    auth.set_access_token(
+        access_token,
+        access_token_secret,
+    )
+    return tweepy.API(auth)
+
+def get_twitter_conn_v2() -> tweepy.Client:
+    """Get twitter conn 2.0"""
+
+    client = tweepy.Client(
+        consumer_key=api_key,
+        consumer_secret=api_secret,
+        access_token=access_token,
+        access_token_secret=access_token_secret,
+    )
+
+    return client
+
